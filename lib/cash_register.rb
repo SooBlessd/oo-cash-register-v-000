457db92d2f8ff86f06839c310efec97348396d1f
Valencia Caldwell <vkcaldwell@gmail.com>
2018-07-03 16:04:38 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,45 @@
+require 'pry'
+class CashRegister
+  attr_accessor :total, :discount, :item, :price, :discounted_amount
+  def initialize(discount = nil)
+    @total = 0
+    @discount = discount
+    @items = []
+    @discounted_amount = discounted_amount
+  end
 
+  def add_item(item, price,quanity = 1)
+    quanity.times do 
+      @items << item 
+    end
+    items_total = price * quanity 
+    @total += items_total
+    # # @price - price 
+    # title = item.new
+  end
+
+  def apply_discount
+    #discount of 20 means 20% off of the total 
+    if @discount >= 0
+      @discounted_amount = @total * self.discount.to_f/100
+      new_total = @total -= discounted_amount
+      "After the discount, the total comes to $#{new_total.to_i}."
+    else 
+       puts "There is no discount to apply."
+    end
+  end
+  
+  def items
+    @items
+  end
+  
+  def void_last_transaction
+    last_item = add_item.pop
+    last_item
+   binding.pry
+    
+  end
+    
+  
+  
+end
